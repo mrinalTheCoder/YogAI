84f42e287cc92723d7d600f7407a0a4120581b3e
Mrinal Jain <2mrinaljain@gmail.com>
2020-10-29 16:14:30 +0530

    deleted camera.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBXPg2s3xi5koHS-W2dgkkMe9r_2EDe4HQ",
+  authDomain: "yogai2020.firebaseapp.com",
+  databaseURL: "https://yogai2020.firebaseio.com",
+  projectId: "yogai2020",
+  storageBucket: "yogai2020.appspot.com",
+  messagingSenderId: "634790331487",
+  appId: "1:634790331487:web:85019a2421a36e101707ea"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
